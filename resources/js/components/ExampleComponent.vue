bcc09b4b2d45fb9104de9d5d19d2831da6a43d7b
Vakhtang Tivadze <vaxotivadze@gmail.com>
2019-10-26 02:03:30 +0400

    Laravel and the Front-end

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header" v-text="name"></div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { name: 'Laracasts' }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style type="text/css" scoped>
+    .card { background: pink; }
+</style>
